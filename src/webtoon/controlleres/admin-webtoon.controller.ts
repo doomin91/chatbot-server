e3ce08e4788db239e87d33d0eb8ab84e5180d10a
cartini-dominic <dominic@cartini.com>
2023-09-10 16:54:04 +0900

    fix. 조회, 추가, 수정, 삭제 API 추가

--- src/webtoon/controlleres/admin-webtoon.controller.ts
+++ src/webtoon/controlleres/admin-webtoon.controller.ts
@@ -1,16 +1,40 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { ApiDoc } from 'src/common/decorators/swagger/api-doc.decorator';
 import { ObjectResponse } from 'src/common/dtos/object-response.dto';
 
-@Controller('admin/webtoon')
-@ApiTags('웹툰 항목 관리')
+@Controller('admin/webtoons')
+@ApiTags('ADMIN 웹툰 항목')
 export class AdminWebtoonController {
   @ApiDoc({
-    summary: '웹툰 항목을 조회합니다.',
+    summary: '웹툰 항목 조회',
   })
   @Get('')
   async findAdminWebtoons(): Promise<ObjectResponse<number>> {
+    return new ObjectResponse(1);
+  }
+
+  @ApiDoc({
+    summary: '웹툰 항목 추가',
+  })
+  @Post('')
+  async insertAdminWebtoons(): Promise<ObjectResponse<number>> {
+    return new ObjectResponse(1);
+  }
+
+  @ApiDoc({
+    summary: '웹툰 항목 수정',
+  })
+  @Put('')
+  async updateAdminWebtoons(): Promise<ObjectResponse<number>> {
+    return new ObjectResponse(1);
+  }
+
+  @ApiDoc({
+    summary: '웹툰 항목 수정',
+  })
+  @Delete('')
+  async deleteAdminWebtoons(): Promise<ObjectResponse<number>> {
     return new ObjectResponse(1);
   }
 }
--- src/webtoon/entities/webtoon.module.ts
+++ src/webtoon/entities/webtoon.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Webtoon } from './webtoon.entity';
+import { AdminWebtoonController } from '../controlleres/admin-webtoon.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Webtoon])],
-  controllers: [],
+  controllers: [AdminWebtoonController],
   providers: [],
 })
 export class WebtoonModule {}
