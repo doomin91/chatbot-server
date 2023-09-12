343d11d5fadea0ccfb846ce335c94d0d948991b1
cartini-dominic <dominic@cartini.com>
2023-09-12 21:34:56 +0900

    fix. 웹툰 정보 불러오기 API 수정

--- src/webtoon-scraper/services/webtoon-scraper.service.ts
+++ src/webtoon-scraper/services/webtoon-scraper.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { ExportScraperService } from 'src/scraper/services/export-scraper.service';
 import * as cheerio from 'cheerio';
-
+import _ from 'lodash';
 @Injectable()
 export class WebtoonScraperService {
   constructor(private exportScraperService: ExportScraperService) {}
 
   async getWebtoonInfo() {
-    const url = 'https://www.naver.com/';
+    const url =
+      'https://comic.naver.com/api/webtoon/titlelist/weekday?order=user';
     const contents = await this.exportScraperService.getContents(url);
+    if (!_.isNil(contents.titleListMap)) {
+      const days = Object.keys(contents.titleListMap);
 
-    const $ = cheerio.load(contents);
-    const $bodyList = $(
-      '#shortcutArea > ul > li > a > span.service_name',
-    ).text();
-
-    console.log($bodyList);
-    // $bodyList.each((i, el) => {
-    //   console.log($(el).find('h3').text());
-    // });
-    // console.log(contents);
-
-    // return contents;
+      for (const day of days) {
+        for (const webtoonItem of contents.titleListMap[day]) {
+          const { titleId } = webtoonItem;
+          console.log(titleId);
+          const url2 = `https://comic.naver.com/api/article/list/info?titleId=${titleId}`;
+          const webtoonDetail = await this.exportScraperService.getContents(
+            url2,
+          );
+          console.log(webtoonDetail);
+        }
+      }
+    }
   }
 }
