51ad26cb36f0e3fce6d23a6d2a2f6c01f8ba81ae
cartini-dominic <dominic@cartini.com>
2023-09-24 19:19:42 +0900

    fix. 불필요한 컨트롤러 삭제

--- src/webtoon-scraper/webtoon-scraper.module.ts
+++ src/webtoon-scraper/webtoon-scraper.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ScraperModule } from 'src/scraper/scraper.module';
-import { WebtoonScraperController } from './controllers/webtoon-scraper.controller';
 import { WebtoonScraperService } from './services/webtoon-scraper.service';
 
 @Module({
   imports: [ScraperModule],
-  controllers: [WebtoonScraperController],
   providers: [WebtoonScraperService],
   exports: [WebtoonScraperService],
 })
