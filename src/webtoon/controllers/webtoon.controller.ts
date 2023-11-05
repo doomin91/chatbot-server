20a5f1dd8dc164ab9ff75605f5cedf19c5d96b76
cartini-dominic <dominic@cartini.com>
2023-11-05 19:42:37 +0900

    test. stringfy

--- src/webtoon/controllers/webtoon.controller.ts
+++ src/webtoon/controllers/webtoon.controller.ts
@@ -7,6 +7,6 @@ import { KakaoRequestDto } from '../dtos/kakao-skill-request-dto';
 export class WebtoonController {
   @Post('test')
   async webtoonTest(req: KakaoRequestDto, res: Response) {
-    return req;
+    return JSON.stringify(req);
   }
 }
