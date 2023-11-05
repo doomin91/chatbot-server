ee47126823c4f21c2c5bcf95abddf17a92ffdf2d
cartini-dominic <dominic@cartini.com>
2023-11-05 19:40:28 +0900

    fix. get -> post

--- src/webtoon/controllers/webtoon.controller.ts
+++ src/webtoon/controllers/webtoon.controller.ts
@@ -1,11 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { KakaoRequestDto } from '../dtos/kakao-skill-request-dto';
 
 @Controller('webtoons')
 @ApiTags('웹툰')
 export class WebtoonController {
-  @Get('test')
+  @Post('test')
   async webtoonTest(req: KakaoRequestDto, res: Response) {
     return req;
   }
